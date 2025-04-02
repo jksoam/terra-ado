aaf895db06e4921966b1d4735018fb6af97d1694
jksoam <jksoam007@gmail.com>
2025-04-02 16:08:12 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features { }
+  subscription_id = "3ace2f6c-7fad-4649-99ca-c60df54b9873"
+}
+
+resource "azurerm_resource_group" "jksoam" {
+  name     = "jksoam"
+  location = "West Europe"
+}
